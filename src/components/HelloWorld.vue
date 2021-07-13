cc771d7a6e6284af0ced17687704595373eb1a1e
BuildTools <lucho.2012.tandil@gmail.com>
2021-07-13 13:13:12 -0300

    actualización de dependencias

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Servidor de minecraft, IP: greenborn.com.ar</h2>
+
+  <div class="container">
+    <h2>Tabs in Vue.js:</h2>
+    <br>
+
+    <ul class="nav nav-tabs nav-justified">
+      <li class="nav-item">
+        <a class="nav-link" @click.prevent="setActive('home')" :class="{ active: isActive('home') }" href="#home">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" @click.prevent="setActive('profile')" :class="{ active: isActive('profile') }" href="#profile">Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" @click.prevent="setActive('contact')" :class="{ active: isActive('contact') }" href="#contact">Contact</a>
+      </li>
+    </ul>
+    <div class="tab-content py-3" id="myTabContent">
+      <div class="tab-pane fade" :class="{ 'active show': isActive('home') }" id="home">
+      <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h2>Servidor de minecraft, IP: greenborn.com.ar</h2>
+        <h3>Por servicio de configuración de servidores Spigot, Paper o Bukkit, puede contactarse conmigo por medio de mi correo electrónico: lucho.2012.tandil[at]gmail.com</h3>
+      </div>
+      </div>
+      <div class="tab-pane fade" :class="{ 'active show': isActive('profile') }" id="profile">Profile content</div>
+      <div class="tab-pane fade" :class="{ 'active show': isActive('contact') }" id="contact">Contact content</div>
+    </div>
   </div>
+
 </template>
 
 <script>
