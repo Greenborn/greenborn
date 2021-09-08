561017e45d01bf3d1c1335c68052ee0a3fd18935
berty <lucho.2012.tandil@gmail.com>
2021-09-08 04:56:17 -0300

    se mueve header a nuevo componente

---
+++ src/app/components/full-header/full-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-header',
+  templateUrl: './full-header.component.html',
+  styleUrls: ['./full-header.component.scss'],
+})
+export class FullHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
