5a6f0e350f9930d69cdcf9c1441e42467b8273b4
berty <lucho.2012.tandil@gmail.com>
2021-10-02 11:33:13 -0300

    Se actualiza el estilo de la sección de servicios

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -13,6 +13,24 @@ export class ServiciosComponent implements OnInit {
       t:"Desarrollo de APP's Moviles", 
       d:"Programamos la APP que necesita, para luego poder exportarla a plataformas Android e IOs, utilizamos Frameworks hibridos como Ionic y React Native.",
       i:"assets/img/icon1.svg"
+    },
+    {
+      id:10, 
+      t:"Web Scraping", 
+      d:"Descargamos información de sitios públicos de su elección para que Ud. pueda disponer de dicha información ya sea en formatos .CSV, Hoja de Cálculo,  Base de datos u otros a determinar.",
+      i:"assets/img/icon2.svg"
+    },
+    {
+      id:30, 
+      t:"Migración Web", 
+      d:"Migramos el contenido de su actual página web, sin importar en que CMS esté desarrollada o otro CMS de su elección, nueva APP Movil o de Escritorio.",
+      i:"assets/img/icon3.svg"
+    },
+    {
+      id:40, 
+      t:"Solución de Errores", 
+      d:"Resolvemos aquellos errores que pudieran estar presentes en el código de su APP o que se deban a defectos de configuración de su servidor.",
+      i:"assets/img/icon4.svg"
     }
   ];
 
