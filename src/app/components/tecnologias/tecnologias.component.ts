d146391d8ecc03a7b5ad8b94d2a997ee0094e09a
berty <lucho.2012.tandil@gmail.com>
2021-11-28 10:05:14 -0300

    Avance en componente de tecnologìas

--- src/app/components/tecnologias/tecnologias.component.ts
+++ src/app/components/tecnologias/tecnologias.component.ts
@@ -7,6 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnologiasComponent implements OnInit {
 
+  public listado_tecnologias_leng:any = [
+    {
+      id:0, 
+      t:"PHP", 
+      i:"assets/img/php.svg"
+    },
+    {
+      id:10, 
+      t:"HTML", 
+      i:"assets/img/html.svg"
+    },
+    {
+      id:30, 
+      t:"JavaScript", 
+      i:"assets/img/js.svg"
+    },
+    {
+      id:40, 
+      t:"TypeScript", 
+      i:"assets/img/ts.svg"
+    },
+    {
+      id:50, 
+      t:"Python", 
+      i:"assets/img/py.svg"
+    },
+    {
+      id:60, 
+      t:"Java", 
+      i:"assets/img/java.svg"
+    },
+    {
+      id:70, 
+      t:"Django", 
+      i:"assets/img/dj.svg"
+    },
+    {
+      id:80, 
+      t:"Maria DB", 
+      i:"assets/img/mariadb.svg"
+    },
+    {
+      id:90, 
+      t:"WordPress", 
+      i:"assets/img/wp.svg"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {}
