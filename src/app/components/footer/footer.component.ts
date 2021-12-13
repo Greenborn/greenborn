fec28ddd0d7a39906539420b8e617ac49cf42810
berty <lucho.2012.tandil@gmail.com>
2021-12-13 09:52:51 -0300

    Actualización del footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,8 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public anio:number = 0;
+  public listado_contactos:any = [
+    {
+      id:0, 
+      t:"Facebook", 
+      i:"assets/img/facebook.svg",
+      e:"https://www.facebook.com/Greenborn-100550959124104"
+    },
+    {
+      id:10, 
+      t:"Instagram", 
+      i:"assets/img/instagram.svg",
+      e:"https://www.instagram.com/greenborn_soft/"
+    },
+    {
+      id:30, 
+      t:"Discord", 
+      i:"assets/img/discord.svg",
+      e:"https://discord.gg/ZH36cEh2"
+    },
+    {
+      id:40, 
+      t:"Linkedin", 
+      i:"assets/img/linkedin.svg",
+      e:"https://www.linkedin.com/company/greenborn"
+    },
+  ];
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.anio = Number(new Date().getFullYear());
+  }
 
 }
