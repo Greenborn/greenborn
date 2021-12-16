bebea1349b593ef4535dcaaf0508eab49322eeac
berty <lucho.2012.tandil@gmail.com>
2021-12-16 10:47:28 -0300

    En formulario de contacto se aplica una valición rudimentaria, y ya hace la petición

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BootstrapFormRequired } from 'src/app/modules/AppUIUtils/components/bootstrap-form/bootstrap-form-validators';
+
+import { BootstrapFormConfig, ButtonBootstrapFormConfig, FieldBootstrapFormConfig } from 'src/app/modules/AppUIUtils/components/bootstrap-form/model/bootstrap-form-config';
 
 import { AppUIUtilsService }   from 'src/app/modules/AppUIUtils/services/app.ui.utils.service';
 import { ContactoForm } from './models/contacto.form';
+import { ContactTypeService } from './services/contact.type.service';
 import { ContactoService } from './services/contacto.service';
 
 @Component({
@@ -11,16 +15,101 @@ import { ContactoService } from './services/contacto.service';
 })
 export class ContactoComponent implements OnInit {
 
+  public formConfig:BootstrapFormConfig = new BootstrapFormConfig();
+
   public contactModel:ContactoForm = new ContactoForm();
 
+  private formSubmit:any          = null;
+  private formIsValidatedSJ:any;
+  private submitBtn:ButtonBootstrapFormConfig;
+
   constructor(
-    private appUIUtilsService: AppUIUtilsService,
-    private contactoService:   ContactoService
+    private appUIUtilsService:  AppUIUtilsService,
+    private contactoService:    ContactoService,
+    private contactTypeService: ContactTypeService
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setFormConfig();
+  }
 
   sendMessage(){
-    this.appUIUtilsService.showMessage("Funcionalidad pendiente de implementación, en breve podrá usar el formulario de contacto");
+    if (this.contactModel.name == ''){
+      this.appUIUtilsService.showMessage("Se debe ingresar un Nombre.");
+      return;
+    }
+
+    if (this.contactModel.message == ''){
+      this.appUIUtilsService.showMessage("Se debe ingresar un Mensaje.");
+      return;
+    }
+
+    if (this.contactModel.email == '' && this.contactModel.tel == ''){
+      this.appUIUtilsService.showMessage("Se debe ingresar un Email o un Télefono.");
+      return;
+    }
+
+    this.appUIUtilsService.presentLoading();
+    this.contactoService.post(this.contactModel).subscribe(
+      ok => {
+        this.appUIUtilsService.dismissLoading();
+        this.appUIUtilsService.showMessage("Mensaje enviado, a la brevedad será contactado.");
+      },
+      err => {
+        this.appUIUtilsService.dismissLoading();
+        this.appUIUtilsService.showMessage("Limite de mensajes superado, intente luego.");
+      }
+    );
   }
+
+  setFormConfig(){
+    //CONFIGURACION INICIAL DEL FORMULARIO
+    /*this.formConfig.clearFields();
+    this.formConfig.setTitle( '' );
+    this.formConfig.model = this.contactModel;
+
+    this.formConfig.AddElement( new FieldBootstrapFormConfig(
+      { title:'Nombre:', field: 'name', type: 'text','extraClass':'col-auto',
+        validator: new BootstrapFormRequired()
+      } ) );
+
+    this.formConfig.AddElement( new FieldBootstrapFormConfig(
+      { title:'Apellido:', field: 'name', type: 'text','extraClass':'col-auto',
+      } ) );
+
+    this.formConfig.AddElement( new FieldBootstrapFormConfig(
+      { title:'Contacto:', field: 'contact_type', type: 'select',
+          validator: new BootstrapFormRequired(),'extraClass':'col-auto',
+          originDataSubject:this.contactTypeService.dataForSelectKeysSubj, provider: this.contactTypeService, getDataFunction:'getListadoKeysSelect'
+      } ) );
+
+    this.formConfig.AddElement( new FieldBootstrapFormConfig(
+      { title:'Cuenta:', field: 'name', type: 'text','extraClass':'col-auto',
+      } ) );
+
+    this.formConfig.AddElement( new FieldBootstrapFormConfig(
+      { title:'Email:', field: 'email', type: 'text','extraClass':'col-auto',
+      } ) );
+
+    this.submitBtn = new ButtonBootstrapFormConfig( { title:'Aplicar', type: 'button' } );
+    this.formConfig.AddElement( this.submitBtn );
+
+    //EVENTOS DEL FORMULARIO
+    //BOTON enviar mensaje
+    this.formSubmit = this.submitBtn.onClick.subscribe({  next: ( params: any ) => {
+      this.formConfig.validateForm.next();
+    } });
+
+    //subscripción a subject de validación de formulario
+    this.formIsValidatedSJ = this.formConfig.isValidated.subscribe({  next: ( params: any ) => {
+      if ( params.success == true ){
+        //Esta todo OK, se puede hacer peticiòn para consultar datos
+        this.appUIUtilsService.presentLoading();
+      } else {
+        this.appUIUtilsService.showMessage( this.appUIUtilsService.getMessageFErrors( params.errors ) );
+      }
+  } });
+  */
+  }
+
 }
--- src/app/components/contacto/services/contact.type.service.ts
+++ src/app/components/contacto/services/contact.type.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { ApiService } from 'src/services/api.service';
 import { ConfigService } from 'src/services/config/config.service';
 import { ContactoForm } from '../models/contacto.form';
@@ -7,11 +8,11 @@ import { ContactoForm } from '../models/contacto.form';
 @Injectable({
   providedIn: 'root'
 })
-export class ContactoService extends ApiService<ContactoForm>{
+export class ContactTypeService extends ApiService<ContactoForm>{
 
   constructor( http: HttpClient,
     config: ConfigService) {
-      super('contact-message', http, config)
+      super('contactoForm', http, config)
      }
     
     get template(): ContactoForm {
@@ -24,4 +25,6 @@ export class ContactoService extends ApiService<ContactoForm>{
             datetime:null,
         }
     }
+
+  dataForSelectKeysSubj:Subject<any> = new Subject();
 }
