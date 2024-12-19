f322fab4eafce9a4375b31a1312dca5408d45c4d
luciano <lucho.2012.tandil@gmail.com>
2024-12-19 00:17:10 -0300

    Se mejora presentacion de servicios, luego se agregaran imagenes

--- src/components/MetodologiaTrabajo.vue
+++ src/components/MetodologiaTrabajo.vue
@@ -6,174 +6,136 @@
                 <div class="col-12 col-sm-8 col-md-6">
 
                     <div class="row row-cmp">
-                        <div class="col-auto">
-                            <div class="circ-met circ-col-1">1</div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-auto"><div class="circ-blend"><div class="circ-met circ-col-1">1</div></div></div>
+                            </div>
                         </div>
 
-                        <div class="col">
-                            <div class="des-cnt">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-tit">Entrevista Inicial</div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-cnt">
-                                            Nos entrevistaremos con Ud. para clarificar las necesidades, relevar requerimientos y 
-                                            dimensionar el alcance del proyecto. 
-                                        </div>
-                                    </div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="des-tit">Entrevista Inicial</div>
                                 </div>
                             </div>
-                            
+                            <div class="des-cnt">
+                                Nos entrevistaremos con Ud. para clarificar las necesidades, relevar requerimientos y 
+                                dimensionar el alcance del proyecto. 
+                            </div>
                         </div>
                     </div>
 
                     <div class="row row-cmp">
-
-                        <div class="col">
-                            <div class="des-cnt">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-tit text-right">Propuesta</div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-cnt text-right">
-                                            A partir del relavamiento de la Entrevista, nos enfocamos en la creación de una
-                                            propuesta de desarrollo, indicando las tecnologías a utilizar, así como su diseño
-                                            y arquitectura.
-                                        </div>
-                                    </div>
-                                </div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-auto"><div class="circ-blend"><div class="circ-met circ-col-2">2</div></div></div>
                             </div>
                         </div>
 
-                        <div class="col-auto">
-                            <div class="circ-met circ-col-2">2</div>
-                        </div>
-
-                    </div>
-
-
-                    <div class="row row-cmp">
-                        <div class="col-auto">
-                            <div class="circ-met circ-col-3">3</div>
-                        </div>
-
-                        <div class="col">
-                            <div class="des-cnt">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-tit">Preparación
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-cnt">
-                                            Montamos un sitio de pruebas que mantendremos actualizado periodicamente.<br>
-                                            En donde podrá constatar el avance del proceso así como da la oportunidad
-                                            de identificar correcciones.
-
-                                            Definimos el Producto Mínimo Viable, el cual será la base que iremos expandiendo.
-                                            
-                                            Definiremos un cronograma de reuniones donde coordinar los avances y un cronograma
-                                            de objetivos de entregas.<br>
-                                        </div>
-                                    </div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="des-tit">Propuesta</div>
                                 </div>
                             </div>
-                            
-                        </div>
-                    </div>
-
-
-                    <div class="row row-cmp">
-
-                        <div class="col">
                             <div class="des-cnt">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-tit text-right">Desarrollo</div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-cnt text-right">
-                                            Damos inicio al proceso de implementación de diseños y arquitecturas propuestos. <br>
-                                            Mantenemos actualizado el sitio de pruebas en cada avance. <br>
-                                            Nos mantendremos en contacto duranto todo el proceso, con el nivel el feedback que Ud. considere.<br>
-                                        </div>
-                                    </div>
-                                </div>
+                                A partir del relavamiento de la Entrevista, nos enfocamos en la creación de una
+                                propuesta de desarrollo, indicando las tecnologías a utilizar, así como su diseño
+                                y arquitectura.
                             </div>
-                        </div>
-
-                        <div class="col-auto">
-                            <div class="circ-met circ-col-4">4</div>
-                        </div>
-
-                    </div>
-
-                    <div class="row row-cmp">
-                        <div class="col-auto">
-                            <div class="circ-met circ-col-5">5</div>
-                        </div>
-
-                        <div class="col">
-                            <div class="des-cnt">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-tit">Implementación</div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-cnt">
-                                            Con cada objetivo complido y validado, requerimos el pago del mismo.<br>
-                                            Realizamos la instalación del sistema en su ubicación definitiva.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            
                         </div>
                     </div>
 
                     <div class="row row-cmp">
-
-                        <div class="col">
-                            <div class="des-cnt">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-tit text-right">Soporte</div>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="des-cnt text-right">
-                                            Luego de la implementación pueden surgir dudas sobre el uso, 
-                                            la necesidad de nuevas funcionalidades o la corrección de bugs.<br>
-                                            Por lo que brindamos soporte post-venta.
-                                        </div>
-                                    </div>
-                                </div>
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-auto"><div class="circ-blend"><div class="circ-met circ-col-3">3</div></div></div>
                             </div>
                         </div>
 
-                        <div class="col-auto">
-                            <div class="circ-met circ-col-6">6</div>
+
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="des-tit">Preparación</div>
+                                </div>
+                            </div>
+                            <div class="des-cnt">
+                                Montamos un sitio de pruebas que mantendremos actualizado periodicamente.<br>
+                                En donde podrá constatar el avance del proceso así como da la oportunidad
+                                de identificar correcciones.
+
+                                Definimos el Producto Mínimo Viable, el cual será la base que iremos expandiendo.
+                                
+                                Definiremos un cronograma de reuniones donde coordinar los avances y un cronograma
+                                de objetivos de entregas.<br>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row row-cmp">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-auto"><div class="circ-blend"><div class="circ-met circ-col-4">4</div></div></div>
+                            </div>
                         </div>
 
+
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="des-tit">Desarrollo</div>
+                                </div>
+                            </div>
+                            <div class="des-cnt">
+                                Damos inicio al proceso de implementación de diseños y arquitecturas propuestos. <br>
+                                Mantenemos actualizado el sitio de pruebas en cada avance. <br>
+                                Nos mantendremos en contacto duranto todo el proceso, con el nivel el feedback que Ud. considere.<br>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row row-cmp">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-auto"><div class="circ-blend"><div class="circ-met circ-col-5">5</div></div></div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="des-tit">implementación</div>
+                                </div>
+                            </div>
+                            <div class="des-cnt">
+                                Con cada objetivo complido y validado, requerimos el pago del mismo.<br>
+                                Realizamos la instalación del sistema en su ubicación definitiva.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row row-cmp">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-auto"><div class="circ-blend"><div class="circ-met circ-col-6">6</div></div></div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="des-tit">Soporte</div>
+                                </div>
+                            </div>
+                            <div class="des-cnt">
+                                Luego de la implementación pueden surgir dudas sobre el uso, 
+                                la necesidad de nuevas funcionalidades o la corrección de bugs.<br>
+                                Por lo que brindamos soporte post-venta.
+                            </div>
+                        </div>
                     </div>
 
                 </div>
@@ -191,7 +153,7 @@ import Notas1 from "../assets/img/notas1.jpeg"
 import Compu2 from "../assets/img/compu2.jpeg"
 </script>
 
-<style scoped>
+<style scss scoped>
 .row-cmp{ 
     margin-top: 5rem;
     margin-bottom: 15rem;
@@ -228,31 +190,64 @@ import Compu2 from "../assets/img/compu2.jpeg"
 }
 
 
-.circ-met{
+.circ-blend, .circ-met{
+    position: absolute;
     width: 15rem;
     height: 15rem;
     border-radius: 50%;
-    
+    transition-duration: 1s;
+}
+
+.circ-met{
     font-size: 6rem;
     text-align: center;
     vertical-align: middle;
     display: table-cell;
+    z-index: 100;
+}
+
+.circ-blend:hover .circ-met{
+    width: 20rem;
+    height: 20rem;
+    font-size: 8rem;
+}
+
+.circ-blend:hover {
+    width: 20rem;
+    height: 20rem;
+}
+
+.circ-blend{
+    mix-blend-mode: difference;
+    z-index: 500;
+    background: #fff;
+    top: -7rem;
+    left: -3rem;
 }
 
 .des-cnt{
     font-size: 2rem;
+    z-index: 300;
+    position: relative;
 }
 
 .des-tit{
     font-size: 4rem;
+    z-index: 300;
+    position: relative;
 }
 
 .text-metod-1 {
     font-size: 2rem;
 }
 
-.carousel-caption{
-    background-color: rgba(0, 0, 0, 0.9);
-    padding:1rem;
+@media (max-width: 353px) {
+    .des-cnt{
+        font-size: 1rem;
+    }
+
+    .des-tit{
+        font-size: 2rem;
+    }
 }
 </style>
