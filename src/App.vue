53e15773d169eabd72434eeb45e0711aad66590f
luciano <lucho.2012.tandil@gmail.com>
2025-06-14 16:52:15 -0300

    [#258] Se agrega componente de whatsapp

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 <CustomCursor :config='{ highlighted_tags: [ "a", "button", ".text-res-1", ".circ-met", ".text-effect", ".menu-item" ], 
                             transform_time: ".2s", click_effect_enabled: false }' />
 <MenuLateralFlotante />
+<WhatsAppWidget phone="542494243387"/>
 </template>
 
 <script setup>
